d7e76b1df880221d6e2065bb5b0f0f4d5f40524e
EBolinVA <elizabeth.bolin@gmail.com>
2023-02-12 11:33:41 -0500

    update logicStep1.js file

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -21,38 +21,38 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    "Street": streets,
-    "Satellite Streets": satelliteStreets
+    "Streets": streets,
+    "Satellite": satelliteStreets
   };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [43.7, -79.3],
-    zoom: 11,
-    layers: [satelliteStreets]
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
 })
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
 
-// Accessing the Toronto airline routes GeoJSON URL
-let torontoHoods = "https://raw.githubusercontent.com/EBolinVA/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json"
+// Retrieve the earthquake GeoJSON data
+let earthquakeData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 // Create a style for the lines.
-let myStyle = {
-    color: "#ffffa1",
-    fillColor: 'light yellow',
-    weight: 2
-}
+//let myStyle = {
+    //color: "#ffffa1",
+    //fillColor: 'light yellow',
+    //weight: 2
+//}
 
 // Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
+d3.json(earthquakeData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data, {
-    style: myStyle,
-    onEachFeature: function(feature, layer) {
-        layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>") 
+    //style: myStyle,
+    //onEachFeature: function(feature, layer) {
+        //layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>") 
             }
             
         })
